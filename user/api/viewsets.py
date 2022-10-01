3cc7ac3485d1de549c29f6c7fc05ed5e08fe0855
AhabweGilbert <92158385+AhabweGilbert@users.noreply.github.com>
2022-09-30 23:01:24 -0700

    Update viewsets.py

--- user/api/viewsets.py
+++ user/api/viewsets.py
@@ -3,6 +3,7 @@ from .serializers import userSerializers
 from django.contrib.auth.models import User
 
 
+
 class userviewsets(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = userSerializers
